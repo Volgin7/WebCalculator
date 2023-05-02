8bb5708db70614eecdb451f47bbecfd5b87e5088
Volgin7 <8612020@gmail.com>
2023-05-02 21:38:25 +0300

    + webCalculator corrected

--- src/main/java/pro/sky/webcalculator/CalculatorController.java
+++ src/main/java/pro/sky/webcalculator/CalculatorController.java
@@ -1,10 +1,12 @@
 package pro.sky.webcalculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
@@ -12,26 +14,39 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
 
     }
-    @GetMapping("/calculator")
+    @GetMapping()
     public String hello() {
-        return calculatorService.hello();
+        return "Добро пожаловать в калькулятор";
     }
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     public String sum(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.sum(num1, num2);
+        return String.valueOf(num1) +
+                " + " + String.valueOf(num2) +
+                " = " + String.valueOf(calculatorService.sum(num1, num2));
     }
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
     public String sub(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.sub(num1, num2);
+        return String.valueOf(num1) +
+                " - " + String.valueOf(num2) +
+                " = " + String.valueOf(calculatorService.sub(num1, num2));
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     public String mult(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.mult(num1, num2);
+        return String.valueOf(num1) +
+                " * " + String.valueOf(num2) +
+                " = " + String.valueOf(calculatorService.mult(num1, num2));
     }
-        @GetMapping("/calculator/divide")
-        public String div(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-            return calculatorService.div(num1, num2);
+    @GetMapping("/divide")
+    public String div(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        if(num2 == 0) {
+            return "Ошибка: делить на 0 нельзя";
+        } else {
+            return String.valueOf(num1) +
+                    " / " + String.valueOf(num2) +
+                    " = " + String.valueOf(calculatorService.div(num1, num2));
         }
+
+    }
 
 }
--- src/main/java/pro/sky/webcalculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/webcalculator/CalculatorServiceImpl.java
@@ -4,37 +4,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
-    public String hello() {
-        return "Добро пожаловать в калькулятор";
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+    public int sub(int num1, int num2) {
+        return num1 - num2;
     }
 
-    public String sum(int num1, int num2) {
-        return String.valueOf(num1) +
-                " + " + String.valueOf(num2) +
-                " = " + String.valueOf(num1 + num2);
-    }
-    public String sub(int num1, int num2) {
-        return String.valueOf(num1) +
-                " - " + String.valueOf(num2) +
-                " = " + String.valueOf(num1 - num2);
+    public int mult(int num1, int num2) {
+        return num1 * num2;
     }
 
-    public String mult(int num1, int num2) {
-        return String.valueOf(num1) +
-                " * " + String.valueOf(num2) +
-                " = " + String.valueOf(num1 * num2);
+    public int div(int num1, int num2) {
+        return num1 / num2;
     }
 
-    public String div(int num1, int num2) {
-        if(num2 != 0) {
-            return String.valueOf(num1) +
-                    " / " + String.valueOf(num2) +
-                    " = " + String.valueOf(num1 / num2);
-        }
-        else {
-            return "Ошибка: делить на 0 нельзя";
-        }
-
-    }
 
 }
